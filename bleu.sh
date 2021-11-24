abc10678260ae3c782262007f7e4d0ebb1a40487
Jordi Mas <jmas@softcatala.org>
2021-11-24 17:33:35 +0100

    Fixes

--- bleu.sh
+++ bleu.sh
@@ -5,13 +5,14 @@ srcLanguage=${BASH_REMATCH[1]}
 tgtLanguage=${BASH_REMATCH[2]}
 
 # test data set
-echo "Test data set" > bleu.txt
 onmt-main --config data.yml --auto_config infer --features_file src-test.txt.token > predictions.txt.token
-perl ../multi-bleu.perl tgt-test.txt.token < predictions.txt.token 
+#perl ../multi-bleu.perl tgt-test.txt.token < predictions.txt.token 
 python3 ../sentencepiece-bleu.py
+echo "multi-bleu.perl" > bleu.txt
 perl ../multi-bleu.perl tgt-test.txt < predictions.txt >> bleu.txt
+echo "sacrebleu" >> bleu.txt
 sacrebleu tgt-test.txt -i predictions.txt -m bleu >> bleu.txt
-
 # Gleu
-../../../gec-training/gleu/compute_gleu -s src-test.txt -r tgt-test.txt -o predictions.txt
+echo "GLEU" >> bleu.txt
+../../../gec-training/gleu/compute_gleu -s src-test.txt -r tgt-test.txt -o predictions.txt >> bleu.txt
 cat bleu.txt
