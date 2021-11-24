c89addcce25ca7880e73912c9511004163eaa3a4
Jordi Mas <jmas@softcatala.org>
2021-11-24 14:58:39 +0100

    Fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 mv ../nmt-models/languages/eng-cat/ ../nmt-models/languages/eng-eng/
 cp bleu.sh  ../nmt-models/languages/eng-eng/
 cp *.yml  ../nmt-models/languages/eng-eng/
-cp *.yml  ../nmt-models/languages/eng-eng/
 cp preprocess.sh  ../nmt-models/languages/eng-eng/
 cp src.txt ../nmt-models/languages/eng-eng/src-raw.txt
 cp tgt.txt ../nmt-models/languages/eng-eng/tgt-raw.txt
