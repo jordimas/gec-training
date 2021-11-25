091bb703d1e9822a570f2394283af0348493f3d8
Jordi Mas <jmas@softcatala.org>
2021-11-25 19:33:20 +0100

    Install

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ cp *.yml  ../nmt-models/languages/eng-eng/
 cp preprocess.sh  ../nmt-models/languages/eng-eng/
 cp src.txt ../nmt-models/languages/eng-eng/src-raw.txt
 cp tgt.txt ../nmt-models/languages/eng-eng/tgt-raw.txt
+cp join-single-file.py ../nmt-models/data-processing-tools/
